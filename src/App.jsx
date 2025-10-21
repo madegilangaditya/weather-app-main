c2ad6cc1e9d1f8a208cd962f08df5555b2ae74f6
gilang aditya <madegilangaditya32@gmail.com>
2025-10-21 14:19:14 +0800

    continue mobile front end html

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import iconDropdown from './assets/images/icon-dropdown.svg'
 import iconSunny from './assets/images/icon-sunny.webp'
 import iconSearch from './assets/images/icon-search.svg'
 import bgTodaySmall from './assets/images/bg-today-small.svg'
+import bgTodayLarge from './assets/images/bg-today-large.svg'
 
 function App() {
 
@@ -56,8 +57,9 @@ function App() {
       </section>
       <section className='main-content'>
         <div className="container">
-          <div className="main-weather-wrap grid gap-3.5 grid-cols-2">
+          <div className="main-weather-wrap grid gap-3.5 grid-cols-2 mb-8">
             <div className="grid-item highlighted rounded-3xl relative overflow-hidden col-span-2 px-6 py-11">
+              <img src={bgTodayLarge} alt="" className='hidden lg:block absolute w-full h-full object-cover -z-10 top-0 left-0' />
               <img src={bgTodaySmall} alt="" className='lg:hidden absolute w-full h-full object-cover -z-10 top-0 left-0' />
               <div className="content-wrap">
                 <div className="title-wrap text-center text-white mb-6">
@@ -70,142 +72,142 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="grid-item">
-              <h3>Feels Like</h3>
-              <span>64°</span>
+            <div className="grid-item p-4 rounded-2xl bg-neutral-800 text-neutral-200 flex flex-col gap-5">
+              <h3 className='text-lg font-normal'>Feels Like</h3>
+              <span className='text-3xl text-neutral-0'>64°</span>
             </div>
-            <div className="grid-item">
-              <h3>Humidity</h3>
-              <span>46%</span>
+            <div className="grid-item p-4 rounded-2xl bg-neutral-800 text-neutral-200 flex flex-col gap-5">
+              <h3 className='text-lg font-normal'>Humidity</h3>
+              <span className='text-3xl text-neutral-0'>46%</span>
             </div>
-            <div className="grid-item">
-              <h3>Wind</h3>
-              <span>9 mph</span>
+            <div className="grid-item p-4 rounded-2xl bg-neutral-800 text-neutral-200 flex flex-col gap-5">
+              <h3 className='text-lg font-normal'>Wind</h3>
+              <span className='text-3xl text-neutral-0'>9 mph</span>
             </div>
-            <div className="grid-item">
-              <h3>Precipitation</h3>
-              <span>0 in</span>
-            </div>
-          </div>
-          <div className="forecast">
-            <h3>Daily forecast</h3>
-            <div className="grid-wrap">
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
-              <div className="forecast-item">
-                <p className='day'>Tue</p>
-                <img src={iconSunny} alt="" srcset="" />
-                <div className="degree-wrap">
-                  <span>68°</span>
-                  <span>57°</span>
-                </div>
-              </div>
+            <div className="grid-item p-4 rounded-2xl bg-neutral-800 text-neutral-200 flex flex-col gap-5">
+              <h3 className='text-lg font-normal'>Precipitation</h3>
+              <span className='text-3xl text-neutral-0'>0 in</span>
             </div>
           </div>
-          <div className="hourly-forecast">
-            <div className="title-wrap">
-              <h3>Hourly forecast</h3>
-              <div className="day">Tuesday</div>
+          <div className="forecast mb-8">
+            <h3 className='text-white text-xl font-semibold mb-5'>Daily forecast</h3>
+            <div className="grid-wrap grid grid-cols-3 gap-4">
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
+              <div className="forecast-item bg-neutral-800 flex flex-col justify-between gap-4 text-center rounded-xl text-white items-center px-2 py-4">
+                <p className='day'>Tue</p>
+                <img src={iconSunny} alt="" srcset="" className='w-14' />
+                <div className="degree-wrap flex justify-between items-center w-full">
+                  <span>68°</span>
+                  <span>57°</span>
+                </div>
+              </div>
             </div>
-            <div className="hourly-weather">
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+          </div>
+          <div className="hourly-forecast bg-neutral-800 rounded-3xl px-4 py-5 mb-8">
+            <div className="title-wrap text-white flex justify-between items-center mb-4">
+              <h3 className='text-xl font-semibold'>Hourly forecast</h3>
+              <div className="day flex justify-between items-center gap-2 rounded-lg bg-neutral-700 px-4 py-1.5 text-sm">Tuesday <img src={iconDropdown} alt="" srcset="" /></div>
+            </div>
+            <div className="hourly-weather flex flex-col gap-4">
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7'/>
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
               </div>
-              <div className="weather-item">
-                <div className="time-wrap">
-                  <img src={iconSunny} alt="" srcset="" />
+              <div className="weather-item bg-neutral-700 rounded-lg flex items-center justify-between px-3 py-5 text-white">
+                <div className="time-wrap flex gap-4 items-center">
+                  <img src={iconSunny} alt="" srcset="" className='w-7' />
                   <span>3 PM</span>
                 </div>
                 <div className="degree">68°</div>
